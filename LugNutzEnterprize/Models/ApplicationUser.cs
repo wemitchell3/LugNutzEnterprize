20e503aec856b09b4677012e098695988d0072fc
William Mitchell <wemitchell3@gmail.com>
2019-07-31 11:26:09 -0500

    adding initial project setup files

---
+++ LugNutzEnterprize/Models/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LugNutzEnterprize.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+    }
+}
